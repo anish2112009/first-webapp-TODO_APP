9708b81473b4005453356041f89809886dfa0bb5
anish <anishraj.ar21@gmail.com>
2023-01-09 19:33:32 +0530

    layout is ready to use

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require(`express`);
+const expresslayouts=require(`express-ejs-layouts`); //to use layouts
 
 
 const app=express();
@@ -7,8 +8,14 @@ const port=27017;
 app.set(`view engine`,`ejs`);
 app.set(`views`,`./views`);  // setting up view engine
 
-app.use(`/`,require(`./routes/index.js`));
 
+
+app.use(expresslayouts);
+
+app.set(`layout extractStyles`,true); // this will allow layout to automatically put the css files within its head for which this layout is called
+app.set(`layout extractScript`,true); // this is for javascript
+
+app.use(`/`,require(`./routes/index.js`));
 app.listen(port,(err)=>{
 
     if(err){
